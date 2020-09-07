b9a019a83c4b085f7bfbb5b9e78c25369b3b7019
aryant <aryantpratapsingh@outlook.com>
2020-09-07 22:30:16 +0530

    30daysofcode

--- 41_FirstMissingPositive_sep7.py
+++ 41_FirstMissingPositive_sep7.py
@@ -1,5 +1,11 @@
 """
 link - https://leetcode.com/problems/first-missing-positive/
+
+Statement - Given an unsorted integer array, find the smallest missing positive integer.
+
+Example 1:
+    Input: [1,2,0]
+    Output: 3
 """
 
 
@@ -15,13 +21,11 @@ def firstMissingPositive(nums):
     while index <= maxi:
         #print(f'index : {index} and result : {result}')
         if index not in nums:
-            result = index
-            break
+            return index
         else:
             index += 1
     if index > maxi:
-        result = maxi + 1
-    return result
+        return maxi + 1
 nums = [-5]#[1,2,0]#[7,8,9,11,12]
 print(firstMissingPositive(nums))
 
