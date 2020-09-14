1efead282537649adbc7cb8c58ef1683e361ff7e
aryant <aryantpratapsingh@outlook.com>
2020-09-14 23:30:48 +0530

    7thDayOfCode

---
+++ 17_LetterCombinationsofPhoneNumber_sep14.py
@@ -0,0 +1,33 @@
+"""
+link- https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+Statement - Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+            A mapping of digit to letters (just like on the telephone buttons) is given below. 
+            Note that 1 does not map to any letters.
+
+Example:
+    Input: "23"
+    Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+"""
+
+def letterCombinations(digits):
+    if digits == "":
+        return []
+    output_str = [""]
+    dict =     {'2' : 'abc',
+               '3' : 'def',
+               '4':  'ghi',
+               '5' : 'jkl',
+               '6' : 'mno',
+               '7' : 'pqrs',
+               '8' : 'tuv',
+               '9' : 'wxyz'}
+    for i in digits:
+        lst = dict[i]
+        temp = []
+        for char_ in lst:
+            for string in output_str:
+                temp.append(string+char_)
+        output_str = temp
+    return output_str
+                    
+            
