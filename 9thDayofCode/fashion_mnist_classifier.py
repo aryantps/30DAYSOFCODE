d1afb59c2becbc1fded3030c31846feb7419eb86
aryant <aryantpratapsingh@outlook.com>
2020-09-16 19:33:31 +0530

    transformed into modular structure

--- 9thDayofCode/fashion_mnist_classifier.py
+++ 9thDayofCode/fashion_mnist_classifier.py
@@ -2,23 +2,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
+import random
 
+	
+mappings = {0:	'T-shirt/top',
+            1:	'Trouser',
+            2:	'Pullover',
+            3:	'Dress',
+            4:	'Coat',
+            5:	'Sandal',
+            6:	'Shirt',
+            7:	'Sneaker',
+            8:	'Bag',
+            9:	'Ankle boot'}
 ########################################### -IMPORTING DATA- #################################################
+def load_dataset():
+    fashion_mnist = keras.datasets.fashion_mnist
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    return train_images, train_labels, test_images, test_labels
 
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-
-def show_image(index):
-    f = plt.figure(figsize=(15, 10))
-    plt.imshow(train_images[index],f)
+def show_image():
+    train_images, train_labels, test_images, test_labels = load_dataset()
+    fig = plt.figure()
+    for i in range(10):
+        ax = fig.add_subplot(2, 5, i+1)
+        ax.imshow(train_images[i])
+    
     plt.show()
+    #plt.savefig('first9.png')
 
-#show_image(5)
+#show_image()
 
 ########################################### -PREPROCESS DATASET- #################################################
+def preprocessing(train_images,test_images):
+    train_images = train_images.astype('float32')
+    test_images = test_images.astype('float32')
 
-train_images = train_images / 255.0
-test_images = test_images / 255.0
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+    return train_images,test_images
 
 # 3 layers(fully connected network)
 # -1st layer – Input Layer – intake an image(Flatten layer that intakes a multi-dimensional array and produces an array of a single dimension)
@@ -27,35 +49,55 @@ test_images = test_images / 255.0
 
 ########################################### -MODEL GENERATION- #################################################
 
-model = keras.Sequential([keras.layers.Flatten(input_shape = (28,28)), 
-                            keras.layers.Dense(128, activation=tf.nn.relu), 
-                            keras.layers.Dense(128, activation=tf.nn.relu),
-                            keras.layers.Dense(10, activation = tf.nn.softmax)])
+def build_model():
+    model = keras.models.Sequential()
+    model.add(keras.layers.Flatten(input_shape = (28,28)))
+    model.add(keras.layers.Dense(128, activation=tf.nn.relu))
+    model.add(keras.layers.Dense(10, activation = tf.nn.softmax))
+    model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+    return model
 
-model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+######################################## -TRAINING & EVALUATE MODEL- ##########################################
+def model_training():
+    train_images, train_labels, test_images, test_labels = load_dataset()
+    train_images,test_images = preprocessing(train_images,test_images)
+    model = build_model()
+    model.fit(train_images,train_labels,epochs=100,verbose=1)
+    model.save('final_FMNIST_model.h5')
+    test_loss, test_accuracy = model.evaluate(test_images,test_labels)
+    print(f'\n\nTest accuracy is {test_accuracy * 100} %\n')
 
-########################################### -TRAINING MODEL- #################################################
+    
 
-model.fit(train_images,train_labels,epochs=100)
 
-########################################### -EVALUATE MODEL- #################################################
+########################################### -TESTING- #################################################
 
-test_loss, test_accuracy = model.evaluate(test_images,test_labels)
-predictions = model.predict(test_images)
 
-print(f'Test accuracy is {test_accuracy * 100} %')
 
 def predictor(index):
     """
     function to predict specific Pokémon.
     input- index of test_data:
+
     """
+    _,_,test_images,test_labels = load_dataset()
+    model = keras.models.load_model('final_FMNIST_model.h5')
+    predictions = model.predict(test_images)
+
     if np.argmax(predictions[index]) == test_labels[index]:
-        print(f'This was correctly predicted to be a \"{test_labels[index]}\"!')
+        category =test_labels[index]
+        print(f'This was correctly predicted to be a \"{mappings[category]}\"!\n')
     else:
-        print(f'This was incorrectly predicted to be a \"{np.argmax(predictions[index])}\". It was actually a \"{test_labels[index]}\".')
+        predicted_category = np.argmax(predictions[index]) 
+        actual_category = test_labels[index]
+        print(f'This was incorrectly predicted to be a \"{mappings[predicted_category]}\". It was actually a \"{mappings[actual_category]}\".\n')
         return(predictions)
 
-predictor(100)
 
+if __name__ == '__main__':
+    model_training()
+
+    sampleTestIndex = random.sample(range(10000),10)
+    for i in sampleTestIndex:
+        predictor(i)
 
